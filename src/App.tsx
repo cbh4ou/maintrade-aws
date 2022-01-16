f01210577b54493beb3745340530e2dbacd8d20e
David Agustin <davidagustin@Davids-MacBook-Pro.local>
2022-01-15 22:49:07 -0800

    Get linter working

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
-import React, { Component } from 'react';
 import './App.css';
-import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
+
+import { AmplifySignOut, withAuthenticator } from '@aws-amplify/ui-react'
 import Amplify from 'aws-amplify';
+import React, { Component } from 'react';
+
 import aws_exports from './aws-exports';
 Amplify.configure(aws_exports);
 
