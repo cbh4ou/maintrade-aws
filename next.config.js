4e83a0aa9322f29dd4d9f974bb82d92c17615b4e
David Agustin <davidagustin@Davids-MacBook-Pro.local>
2022-01-16 22:53:00 -0800

    Install next-optimized-images

--- next.config.js
+++ next.config.js
@@ -1,16 +1,17 @@
 const withPWA = require('next-pwa')
 const runtimeCaching = require('next-pwa/cache')
+const withOptimizedImages = require('next-optimized-images');
 
 // disabled is commented out for debugging
 
 /** @type {import('next').NextConfig} */
-const nextConfig = withPWA({
+const nextConfig = withPWA(withOptimizedImages({
   reactStrictMode: true,
   pwa: {
     dest: 'public',
     runtimeCaching,
     disable: process.env.NODE_ENV === 'development'
   },
-})
+}))
 
 module.exports = nextConfig
